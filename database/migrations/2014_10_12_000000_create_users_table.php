684eeb461c06347797db6de9b178cface116bf04
ForceTower <joaopaulo761@gmail.com>
2017-02-28 20:15:33 -0300

    User table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,12 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->bigInteger('facebook_id')->unique()->unsigned()->nullable();
             $table->string('password', 60);
+            $table->binary('avatar')->nullable();
+            $table->boolean('activated')->default('true');
+            $table->integer('user_level')->default('0');
+            $table->bigInteger('time_watched')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
